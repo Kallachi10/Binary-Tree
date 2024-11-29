56f3a079d2aca84b2622481d292fb7874feb61cd
johnsaju <jsajukallachiyil@gmail.com>
2024-11-29 16:22:25 +0530

    added postorder and inorder

--- binarytree.c
+++ binarytree.c
@@ -43,24 +43,47 @@ void preOrder(tree* root){/*follows classic recursive algorithm for preorder*/
     preOrder(root->Llink);
     preOrder(root->Rlink);
 }
+void postOrder(tree* root){
+    if(root == NULL)
+        return;
+    postOrder(root->Llink);
+    postOrder(root->Rlink);
+    printf("%d -> ",root->value);
+}
+void inOrder(tree* root){
+    if(root == NULL)
+        return;
+    inOrder(root->Llink);
+    printf("%d -> ",root->value);
+    inOrder(root->Rlink);
+}
 void main(){
     tree *root = NULL;
     while(1){
-        int choice = 0;
-        printf("\n\tTrees-Menu\t\n");
+        
+        printf("\n\t\tTrees-Menu\t\n");
         printf("------------------------------------------");
-        printf("\n1. Create a Tree 2.preorder 3.Exit\n");
+        printf("\n1.Create-a-Tree\t\t2.pre-order\n3.in-order\t\t4.post-order\n5.Exit\t\n");
         printf("------------------------------------------\n");
+        int choice=0;
         scanf("%d",&choice);
 
         switch(choice){
             case 1: insertNode(&root);
                     break;
-            case 2: printf("Preorder: ");
+            case 2: printf("Pre-order: ");
                     preOrder(root);
                     printf("END\n");
                     break;
-            case 3: exit(0);
+            case 3: printf("In-order: ");
+                    inOrder(root);
+                    printf("END\n");
+                    break;
+            case 4: printf("Post-order: ");
+                    postOrder(root);
+                    printf("END\n");
+                    break;
+            case 5: exit(0);
 
             default: printf("Invalid Choice!");
                     break; 
@@ -68,6 +91,6 @@ void main(){
     }
 }
 /*Example to try out: 
-    create a tree(input in order): 5 6 9 -1 -1 8 -1 -1 7 -1 3 2 -1 -1 -1
+    create a tree(input in order): 5 6 9 -1 -1 8 -1 -1 7 -1 3 2 -1 -1 -1.
     preorder: 5 6 9 8 7 3 2 
 */
